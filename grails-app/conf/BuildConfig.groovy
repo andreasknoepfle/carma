cfd2520cdc87747c52bdccfb951158b0b848168d
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-06-05 11:44:43 +0200

    JSON Heroku deps

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,6 +33,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         runtime 'postgresql:postgresql:8.4-702.jdbc3'
+        runtime 'net.sf.json-lib:json-lib:2.2.3'
         // runtime 'mysql:mysql-connector-java:5.1.16'
     }
 
